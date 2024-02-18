ad37b0ac7242406f89823c74720add74972a201a
megawatipepek <144662266+megawatipepek@users.noreply.github.com>
2024-02-18 20:04:35 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '8d0b676e-256c-4114-b351-022af82f2afb';
 
-const proxyIPs = ['98.98.119.110'];
+const proxyIPs = ['51.15.125.124'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
